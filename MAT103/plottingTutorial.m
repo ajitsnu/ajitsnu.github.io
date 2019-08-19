c1a78defcea83beea3ce35bfb36fb5dfa2d6c9b2
Ajit Kumar <ajitkumar22@gmail.com>
2019-08-19 13:38:24 +0530

    MAT103 transformation added

--- MAT103/plottingTutorial.m
+++ MAT103/plottingTutorial.m
@@ -1,13 +1,5 @@
 clc
 clear all
 
-[t,s]=meshgrid(0:pi/10:2*pi,  0:0.1:6);
-
-% a cylinder
-x=cos(t);
-y=sin(t);
-z=s;
-
-surf(x,y,z,'EdgeColor','none')
-%mesh(x,y,z)
-axis([-2 2 -2 2 0 7])
+[x,y]=meshgrid(-3:0.2:3, -3:0.2:3);
+quiver(x,y,-x,-y)
