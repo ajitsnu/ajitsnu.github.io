2892aa7ea10196b22f2088c7f089139b44d65c89
Ajit Kumar <ajitkumar22@gmail.com>
2019-08-29 00:41:49 +0530

    MAT103 updated

--- MAT103/gradients/directionalDerivatives.m
+++ MAT103/gradients/directionalDerivatives.m
@@ -6,44 +6,43 @@ f = @(x,y)(x.^2 +y.^3);
 fx = @(x,y)(2*x);
 fy = @(x,y)(3*y.^2);
 
-% point of evaluation of gradient (a,b)
-a = 2; b = 2;
+% point of evaluation of gradient (x0,y0)
+x0 = 2; y0 = 2;
 
-% in the direction v = (v1,v2)
-v1 = 1; v2 = 2;
+% in the direction v = (a,b)
+a = 1; b = -2;
 
 % plot surface
 h = 2;
-[x,y] = meshgrid(a-h:0.1:a+h,a-h:0.1:a+h);
+[x,y] = meshgrid(x0-h:0.1:x0+h,y0-h:0.1:y0+h);
 z= f(x,y);
-%surf(x,y,z, 'facealpha',0.5, 'edgealpha',0 )
 surf(x,y,z,'edgecolor','interp' )
 grid off
-axis([a-h a+h a-h a+h min(min(z)) max(max(z)) ])
+axis([x0-h x0+h y0-h y0+h min(min(z)) max(max(z)) ])
 xlabel('x'); ylabel('y'); zlabel('f');
 hold on
 
 % plot xy plane
-[x,y] = meshgrid(a-h:1:a+h,a-h:1:a+h);
-mesh(x,y,zeros(size(x)), 'edgecolor','k', 'facealpha', 0.8,'edgealpha',0.5)
+[x,y] = meshgrid(x0-h:1:x0+h,y0-h:1:y0+h);
+mesh(x,y,zeros(size(x)), 'edgecolor','k')
 
 
 % plot tangent plane
-L = @(x,y)(f(a,b) + fx(a,b)*(x-a) + fy(a,b)*(y-b));
+L = @(x,y)(f(x0,y0) + fx(x0,y0)*(x-x0) + fy(x0,y0)*(y-y0));
 %surf(x,y,L(x,y));
 
 % mark point (a,b)
-plot3(a,b,0,'ro','linewidth',2);
-plot3(a,b,f(a,b),'ko','linewidth',2)
+plot3(x0,y0,0,'ro','linewidth',2);
+plot3(x0,y0,f(x0,y0),'ko','linewidth',2)
 
 % line in the direcition of v
 t = linspace(-5,5,500);
-xt = a + v1*t;
-yt = b + v2*t;
+xt = x0 + a*t;
+yt = y0 + b*t;
 zh = zeros(size(xt));
 zf = f(xt,yt);
 zl = L(xt,yt);
-zhh = f(a,b)*ones(size(xt));
+zhh = f(x0,y0)*ones(size(xt));
 plot3(xt,yt,zh,'r','linewidth',2)
 plot3(xt,yt,zf,'k','linewidth',2)
 plot3(xt,yt,zl,'b','linewidth',2)
--- MAT103/gradients/gradientsAndGraphs.m
+++ MAT103/gradients/gradientsAndGraphs.m
@@ -2,21 +2,26 @@ clc
 clear all
 
 % function and its gradient
-f = @(x,y)(-x.^2 -y.^2);
-fx = @(x,y)(-2*x);
-fy = @(x,y)(-2*y.^1);
+%f = @(x,y)(-x.^2 -y.^2);
+%fx = @(x,y)(-2*x);
+%fy = @(x,y)(-2*y.^1);
+
+f = @(x,y)(sin(x) + exp(x.*y));
+fx = @(x,y)(cos(x) + y.*exp(x.*y));
+fy = @(x,y)(x.*exp(x.*y));
 
 % point of evaluation of gradient (a,b)
 a = 0; b = 0;
 
 
 % plot surface
-h = 2;
+h = 1;
 [x,y] = meshgrid(a-h:0.1:a+h,a-h:0.1:a+h);
 z= f(x,y);
-surf(x,y,z,'edgecolor','interp')
+%surf(x,y,z,'edgecolor','interp')
+contour3(x,y,z,100)
 grid off
-axis([a-h a+h a-h a+h min(min(z)) max(max(z)) ])
+axis([a-h a+h b-h b+h min(min(z)) max(max(z)) ])
 xlabel('x'); ylabel('y'); zlabel('f');
 hold on
 
