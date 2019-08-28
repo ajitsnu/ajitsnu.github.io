1a109bb33f03f50f8daab0ce69b9572c3c0fc1f2
Ajit Kumar <ajitkumar22@gmail.com>
2019-08-28 15:25:01 +0530

    gradients added in MAT103

--- MAT103/plottingTutorial.m
+++ MAT103/plottingTutorial.m
@@ -1,17 +1,9 @@
 clc
 clear all
 
+[x,y] = meshgrid(-2*pi:0.5:2*pi,  -2*pi:0.5:2*pi);
+Fx = y;
+Fy = 1./x;
 
-[t,s]=meshgrid(0:pi/100:2*pi,  0:pi/100: 2*pi);
-
-
-a = 0.5;
-b = 3;
-% given surface
-x= (b + a*cos(s)).*cos(t);
-y= (b + a*cos(s)).*sin(t);
-z= a *sin(s);
-
-%surf(x,y,z,'EdgeColor','none')
-mesh(x,y,z, 'linewidth',4)
-%axis([-2 2 -2 2 0 7])
+quiver(x,y,Fx,Fy,1, 'linewidth',2)
+axis('equal')
