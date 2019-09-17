219c464dce5cded8a5587d436903fdb7802e3310
Ajit Kumar <ajitkumar22@gmail.com>
2019-09-17 12:11:03 +0530

    MAT 103 updated

--- MAT103/curvatures.m
+++ MAT103/curvatures.m
@@ -2,7 +2,7 @@ clc
 clear all
 
 syms t
-x = 3*cos(t); y = 3*sin(t); z = t;
+x = t; y = t^2; z = 0*t;
 
 r = [x y z];
 
@@ -14,45 +14,52 @@ N = diff(T,t)./norm(diff(T,t));
 B = cross(T,N);
 
     
-s = linspace(0,4*pi,100);
-xs = subs(x,t,s);
-ys = subs(y,t,s);
-zs = subs(z,t,s);
 
-plot3(xs,ys,zs, 'linewidth',2)
-%axis([-3 3 0 9 -1 1])
-hold on
 
 % lets plot the osculating circles
-s = 3*pi/2;
-p = double(subs(r,t,s))  % point on the curve
-Tp = double(subs(T,t,s));
-Np = double(subs(N,t,s))
-Bp = double(subs(B,t,s));
+for s = -4:0.1:4;
+    
+    ss = linspace(-4,4,100);
+    xs = subs(x,t,ss);
+    ys = subs(y,t,ss);
+    zs = subs(z,t,ss);
 
-quiver3(p(1),p(2),p(3),Tp(1),Tp(2),Tp(3),0,'linewidth',2,'color','m');
-quiver3(p(1),p(2),p(3),Np(1),Np(2),Np(3),0,'linewidth',2,'color','r');
-quiver3(p(1),p(2),p(3),Bp(1),Bp(2),Bp(3),0,'linewidth',2,'color','k');
+    plot3(xs,ys,zs, 'linewidth',2)
+    axis([-10 10 -10 10 -2 2])   
+    view(0,90)
+    hold on
+        
+    p = double(subs(r,t,s))  % point on the curve
+    Tp = double(subs(T,t,s));
+    Np = double(subs(N,t,s))
+    Bp = double(subs(B,t,s));
 
-kp = double(subs(k,t,s))
-cp = p + (1/kp)*Np
+    quiver3(p(1),p(2),p(3),Tp(1),Tp(2),Tp(3),0,'linewidth',2,'color','m');
+    quiver3(p(1),p(2),p(3),Np(1),Np(2),Np(3),0,'linewidth',2,'color','r');
+    quiver3(p(1),p(2),p(3),Bp(1),Bp(2),Bp(3),0,'linewidth',2,'color','k');
 
-t = linspace(0,2*pi,100);
-x = (1/kp)*cos(t);
-y = (1/kp)*sin(t);
-z  = zeros(size(x));
+    kp = double(subs(k,t,s))
+    cp = p + (1/kp)*Np
 
-A = inv([Np' Tp' Bp']);
-xyz = [x;y;z];
-xyz = A*xyz;
-x = xyz(1,:);
-y = xyz(2,:);
-z = xyz(3,:);
+    tt = linspace(0,2*pi,100);
+    xs = (1/kp)*cos(tt);
+    ys = (1/kp)*sin(tt);
+    zs = zeros(size(xs));
 
-%osculating cirlcle
-plot3(x,y,z,'--','linewidth',2)
-view(0,90)
+    A = [Np' Tp' Bp'];
+    xyz = [xs;ys;zs];
+    xyz = A*xyz;
+    xs = xyz(1,:) + cp(1);
+    ys= xyz(2,:) + cp(2);
+    zs= xyz(3,:) + cp(3);
 
+    %osculating cirlcle
+    plot3(xs,ys,zs,'--','linewidth',2)
+
+    pause(0.01);
+    
+    hold off
+end
 
 
 
